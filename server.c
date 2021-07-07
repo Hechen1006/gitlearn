c0c848fde155261f742b3f981316db32f9977b91
Cilin <lizhaonan@star-net.cn>
2021-07-07 16:28:47 +0800

    c code for socket initialization

---
+++ server.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+int main()
+{
+	int sockfd;
+	struct sockaddr_in addr;
+	sockfd=socket(PF_INET,SOCK_STREAM,0);
+	if(sockfd==-1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+	addr.sin_family=PF_INET;
+	addr.sin_port=htons(9999);
+	addr.sin_addr.s_addr=inet_addr("192.168.130.98");
+	memset(&addr.sin_zero,0,sizeof(addr.sin_zero));
+	return 0;
+}
--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-	printf("salut le monde!\n");
+	printf("hello world\n");
 	return 0;
 }
