4e5f701b1fc0df090a17bae62e88c53f3519eb6d
Cilin <lizhaonan@star-net.cn>
2021-07-07 17:12:00 +0800

    bind

--- server.c
+++ server.c
@@ -4,11 +4,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 int main()
 {
 	int sockfd;
 	struct sockaddr_in addr;
+	//socket initialization
 	sockfd=socket(PF_INET,SOCK_STREAM,0);
 	if(sockfd==-1)
 	{
@@ -19,5 +21,12 @@ int main()
 	addr.sin_port=htons(9999);
 	addr.sin_addr.s_addr=inet_addr("192.168.130.98");
 	memset(&addr.sin_zero,0,sizeof(addr.sin_zero));
+	//bind
+	if(bind(sockfd,(struct sockaddr*) &addr,sizeof(struct sockaddr))==-1)
+	{
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+	close(sockfd);
 	return 0;
 }
